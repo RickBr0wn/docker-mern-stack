f993349369d35ebb38cc0a389fb17efc6dc20940
Rick Brown <ricky.brown.00@gmail.com>
2020-08-26 01:42:43 +0100

    adds router

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
 // server.js
-const express = require('express');
-const app = express();
-const PORT = 8080;
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+// import routes
+const postRouter = require('./src/routes/postRouter')
+// database configuration
+require('./src/database')
+
+const PORT = 8080
+
+app.use(bodyParser.json())
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.use('/posts', postRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello World ! ');
-});
+  res.send('Hello World ! ')
+})
 
 app.listen(PORT, function () {
-  console.log(`Server Listening on ${PORT}`);
-});
+  console.log(`Server Listening on ${PORT}`)
+})
